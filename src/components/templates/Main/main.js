3cd7bf341125f8e7c8bb9bd01b3037d70e7f25c3
Bernardo Enock <bernardoenockgm@gmail.com>
2025-09-22 19:36:48 -0300

    feat: Add topMain on main

--- src/components/templates/Main/main.js
+++ src/components/templates/Main/main.js
@@ -1,5 +1,10 @@
 import { Component } from "../../../core/component.js";
+import { TopMain } from "../../organisms/topMain.js";
 
 export function MainTemplate(targetId) {
-  return new Component("main", { attributes: { id: targetId } });
+
+  const topMain = new TopMain();
+  const containerMD = new Component("div", { attributes: { id: targetId, class: "container-md" } });
+
+  return new Component("main", {}, [topMain, containerMD]);
 }
