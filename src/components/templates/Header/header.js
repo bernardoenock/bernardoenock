187ff0ba4461354ba73a6d5e46c926c838d41a12
Bernardo Enock <bernardoenockgm@gmail.com>
2025-09-22 15:05:02 -0300

    feat: Add ToggleButton

--- src/components/templates/Header/header.js
+++ src/components/templates/Header/header.js
@@ -3,6 +3,7 @@ import { SettingsIcon } from "../../atoms/SettingsIcon/settingsIcon.js";
 import { HomeIcon } from "../../atoms/HomeIcon/homeIcon.js";
 import { SearchInput } from "../../atoms/SearchInput/searchInput.js";
 import { Modal } from "../../atoms/Modal/modal.js";
+import { ToggleButton } from "../../atoms/ToggleButton/toggleButton.js";
 import { NavBar } from "../../molecules/Navbar/navbar.js";
 
 export function HeaderTemplate(links) {
@@ -10,14 +11,35 @@ export function HeaderTemplate(links) {
   const searchInput = new SearchInput("Buscar no portfólio...");
   const homeIcon = new HomeIcon();
 
-  const settingsModal = new Modal("settings-modal", "xConfigurações", navBar);
+  const handleToggleTheme = () => {
+    document.body.classList.toggle("dark-theme");
+    const isDark = document.body.classList.contains("dark-theme");
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  };
+
+  const themeToggleButton = new ToggleButton(handleToggleTheme);
+
+  const contantModal = new Component("div", { attributes: { class: "" } }, [navBar, themeToggleButton]);
+
+  const settingsModal = new Modal("settings-modal", "xConfigurações", contantModal);
   const settingsIcon = new SettingsIcon(() => {
     document.getElementById("settings-modal").classList.remove("hidden");
   });
 
+
+
   const containerHeader = new Component("header", { attributes: { class: "header-container" } });
   const containerHomeIcon = new Component("div", { attributes: { class: "header-left" } }, [homeIcon]);
-  const containerSettingsIcon = new Component("div", { attributes: { class: "header-right" } }, [searchInput, settingsIcon, settingsModal]);
+  const containerSettingsIcon = new Component(
+    "div", 
+    { attributes: { class: "header-right" } }, 
+    [
+      searchInput, 
+      settingsIcon,
+      themeToggleButton,
+      settingsModal
+    ]
+  );
 
   return containerHeader.appendChildren([
       containerHomeIcon,
