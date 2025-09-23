93189882917af12cd3ba9b2c13c335c2393aaad4
Bernardo Enock <bernardoenockgm@gmail.com>
2025-09-22 22:36:02 -0300

    feat: Refac TopMain

--- src/components/organisms/topMain.js
+++ src/components/organisms/topMain.js
@@ -1,44 +1,12 @@
 import { Component } from "../../core/component.js";
+import { BackgroundImage } from "../atoms/BackgroundImage/backgroundImage.js";
+import { ProfileWrapper } from "../molecules/ProfileWrapper/profileWrapper.js";
 
 export class TopMain extends Component {
   constructor() {
     super("section", { attributes: { class: "top-main" } }, [
-      // Background image
-      new Component("div", { attributes: { class: "top-main__background" } }, [
-        new Component("img", {
-          attributes: {
-            src: "./assets/imgs/background.jpg",
-            alt: "Background",
-            class: "top-main__background-image",
-          },
-        }),
-      ]),
-
-      // Profile wrapper
-      new Component("div", { attributes: { class: "top-main__profile" } }, [
-        // Profile image
-        new Component("div", { attributes: { class: "top-main__avatar-wrapper" } }, [
-          new Component("img", {
-            attributes: {
-              src: "./assets/imgs/me.jpeg",
-              alt: "Bernardo Enock",
-              class: "top-main__avatar",
-            },
-          }),
-        ]),
-
-        // Profile text info
-        new Component("div", { attributes: { class: "top-main__info" } }, [
-          new Component("h1", { attributes: { class: "top-main__name" } }, ["Bernardo Enock"]),
-          new Component("p", { attributes: { class: ".top-main__position" } }, ["Desenvolvedor Full Stack"]),
-          new Component("p", { attributes: { class: "top-main__headline" } }, [
-            "Developer | RPA | Front | Back | TypeScript | PHP | Python | Java | C# | SQL",
-          ]),
-          new Component("p", { attributes: { class: "top-main__location" } }, [
-            "Brasil",
-          ]),
-        ]),
-      ]),
+      new BackgroundImage(),
+      new ProfileWrapper(),
     ]);
   }
 }
