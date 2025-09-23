6f32908df59afc48f836f5fa6bf5c613a6d296f3
Bernardo Enock <bernardoenockgm@gmail.com>
2025-09-23 14:44:45 -0300

    feat: Add container for Avatar photo

--- src/components/atoms/AvatarWrapper/avatarWrapper.js
+++ src/components/atoms/AvatarWrapper/avatarWrapper.js
@@ -2,15 +2,17 @@ import { Component } from "../../../core/component.js";
 
 export class AvatarWrapper extends Component{
   constructor() {
-    super ("div", { attributes: { class: "top-main__avatar-wrapper" } }, 
+    super ("div", { attributes: { class: "top-main__avatar-container" } }, 
       [
-        new Component("img", {
-          attributes: {
-            src: "./assets/imgs/me.jpeg",
-            alt: "Bernardo Enock",
-            class: "top-main__avatar",
-          },
-        }),
+        new Component("div", { attributes: { class: "top-main__avatar-wrapper" } }, [
+          new Component("img", {
+            attributes: {
+              src: "./assets/imgs/me.jpeg",
+              alt: "Bernardo Enock",
+              class: "top-main__avatar",
+            },
+          }),
+        ])
       ])
   }
 }
