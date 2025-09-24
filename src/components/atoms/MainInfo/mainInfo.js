503570a930f0ff3c89f8d88f593ac991b4dd44cd
Bernardo Enock <bernardoenockgm@gmail.com>
2025-09-23 22:12:32 -0300

    fix: MainInfo

--- src/components/atoms/MainInfo/mainInfo.js
+++ src/components/atoms/MainInfo/mainInfo.js
@@ -5,7 +5,7 @@ export class MainInfo extends Component {
     super("div", { attributes: { class: "top-main__info" } }, [
         new Component("div", { attributes: { class: "top-main__container"}}, [
           new Component("h1", { attributes: { class: "top-main__name" } }, ["Bernardo Enock"]),
-          new Component("p", { attributes: { class: "top-main__position" } }, ["/ Desenvolvedor Full Stack"]),
+          new Component("p", { attributes: { class: "top-main__position" } }, ["Desenvolvedor Full Stack"]),
         ]),
        
         new Component("p", { attributes: { class: "top-main__headline" } }, [
