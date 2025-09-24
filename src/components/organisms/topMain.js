1c1a55dbfc883f4b68cbc614650bed9cde2731a9
Bernardo Enock <bernardoenockgm@gmail.com>
2025-09-23 22:13:41 -0300

    fix: TopMain

--- src/components/organisms/topMain.js
+++ src/components/organisms/topMain.js
@@ -1,12 +1,19 @@
 import { Component } from "../../core/component.js";
-import { BackgroundImage } from "../atoms/BackgroundImage/backgroundImage.js";
 import { ProfileWrapper } from "../molecules/ProfileWrapper/profileWrapper.js";
+import { NavMain } from "../molecules/NavMain/navMain.js";
 
 export class TopMain extends Component {
   constructor() {
+
+    const links = [
+      { page: "home", label: "Home" },
+      { page: "about", label: "Sobre" },
+      { page: "contacts", label: "Contato" }
+    ];
+
     super("section", { attributes: { class: "top-main" } }, [
-      new BackgroundImage(),
       new ProfileWrapper(),
+      new NavMain(links),
     ]);
   }
 }
