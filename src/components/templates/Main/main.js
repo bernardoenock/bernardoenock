614f7797c0cd835c47f3d4ef2660abffe5039ce5
Bernardo Enock <bernardoenockgm@gmail.com>
2025-09-23 22:10:06 -0300

    feat: Change background

--- src/components/templates/Main/main.js
+++ src/components/templates/Main/main.js
@@ -2,9 +2,29 @@ import { Component } from "../../../core/component.js";
 import { TopMain } from "../../organisms/topMain.js";
 
 export function MainTemplate(targetId) {
-
   const topMain = new TopMain();
-  const containerMD = new Component("div", { attributes: { id: targetId, class: "container-md" } });
+  const containerMD = new Component("div", { 
+    attributes: { id: targetId, class: "container-md" } 
+  });
 
-  return new Component("main", {}, [topMain, containerMD]);
+  const containerMain = new Component("section", { 
+    attributes: { class: "container-main" } 
+  }, [
+    topMain,
+    containerMD
+  ]);
+
+  const backgroundVideo = new Component("video", {
+    attributes: { 
+      autoplay: true,
+      muted: true,
+      loop: true,
+      playsinline: true,
+      class: "background-video"
+    }
+  }, [
+    new Component("source", { attributes: { src: "/assets/videos/points.mp4", type: "video/mp4" } })
+  ]);
+
+  return new Component("main", {}, [backgroundVideo, containerMain]);
 }
