ac2bf303a161927f2ced47a3b0b83a6ebcbd1b40
Bernardo Enock <bernardoenockgm@gmail.com>
2025-09-23 22:13:17 -0300

    feat: Add CardBtnsMain on ProfileWrapper

--- src/components/molecules/ProfileWrapper/profileWrapper.js
+++ src/components/molecules/ProfileWrapper/profileWrapper.js
@@ -1,11 +1,13 @@
 import { Component } from "../../../core/component.js";
 import { AvatarWrapper } from "../../atoms/AvatarWrapper/avatarWrapper.js";
 import { MainInfo } from "../../atoms/MainInfo/mainInfo.js";
+import { CardBtnsMain } from "../CardBtnsMain/cardBtnsMain.js";
 
 export class ProfileWrapper extends Component{
   constructor() {
     super("div", { attributes: { class: "top-main__profile" } }, [
       new AvatarWrapper(),
+      new CardBtnsMain(),
       new MainInfo(),
     ]);
   }
